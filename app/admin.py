850aab25aada2c701ff7f519094d8b3c5bde2899
Bruno Sbardellati <160192137+BrunoSbards@users.noreply.github.com>
2025-02-20 23:56:44 -0300

    Delete app directory

--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import estado, matricula, leiloeiro, anexo
-
-admin.site.register(estado)
-admin.site.register(matricula)
-admin.site.register(leiloeiro)
-admin.site.register(anexo)
